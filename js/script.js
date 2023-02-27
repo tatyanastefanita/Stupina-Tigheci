cc94cea31f3ab9bd132fe2b99becf2e8c601f70d
tatyanastefanita <110549354+tatyanastefanita@users.noreply.github.com>
2023-02-27 16:30:55 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+    var x = document.getElementsByTagName("navbar")[0];
+    if (x.className === "") {
+        x.className += "mobile";
+    } else {
+        x.className = "";
+    }
+}
